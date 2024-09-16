194d994f2ea286df2c1a36cd14c792501b8dc7a7
Vitorgz01 <vitorginza123@gmail.com>
2024-09-16 18:58:15 -0300

    terminando de fazer o router e a criação das paginas

---
+++ src/routes/Privacidade/index.tsx
@@ -0,0 +1,42 @@
+export default function Privacidade() {
+    return (
+        <main>
+            <div className="titulo_texto">
+                <h1 className="titulo_privacidade">Política de Privacidade da Porto</h1>
+                <section className="texto_privacidade">
+                    <p>
+                        Nós, do Grupo Porto, formado pela Porto Seguro S.A., inscrita no CNPJ/ME, sob o nº. 02.149.205/0001-69 e pelas empresas que ela controla, direta ou indiretamente, prezando pela tradição do bom atendimento e transparência na forma de tratamento dos dados das pessoas com quem se relaciona, sejam elas clientes, ex-clientes, funcionários, corretores, prestadores de serviços e fornecedores, tanto na contratação de seus produtos e serviços quanto na candidatura às vagas de trabalho ou ainda, com quem mais acessa nossas plataformas digitais, mantemos o compromisso de continuar respeitando e assegurando a privacidade e a proteção dos dados pessoais de quem quer que seja.
+                    </p>
+                    <p>
+                        Por isso, apresentamos nossa Política de Privacidade, que tem como objetivo fornecer a você informações claras e precisas sobre o tratamento de dados pessoais e sensíveis realizados pela Porto ao utilizar seus sites, portais, aplicativos, produtos e serviços.
+                    </p>
+                    
+                    <h2>1. Lei Geral de Proteção de Dados (LGPD)</h2>
+                    <p>
+                        A LGPD, lei federal 13.709/18, que já existe desde 2020, tem como objetivo proteger os direitos fundamentais de liberdade e de privacidade e o livre desenvolvimento da personalidade da pessoa natural.
+                    </p>
+                    <p>
+                        É a unificação das previsões acerca das obrigações e responsabilidades das partes no tratamento de dados pessoais e sensíveis, tanto no mundo físico quanto no digital.
+                    </p>
+                    <p>
+                        As obrigações e responsabilidades previstas na lei se aplicam às pessoas físicas, que realizem o tratamento de dados pessoais para fins econômicos, e pessoas jurídicas de direito público (entidades e órgãos governamentais) e privado.
+                    </p>
+                    <p>
+                        Seus conceitos estão estabelecidos no artigo 5º e tem como principais personagens: o titular de dados (pessoa física viva), o controlador de dados (quem determina a forma de tratamento dos dados), o operador (quem efetua o tratamento dos dados sob as ordens do controlador) e a ANPD (Autoridade Nacional de Proteção de Dados), órgão da administração pública responsável por regular e fiscalizar o cumprimento da LGPD.
+                    </p>
+
+                    <h2>2. Coleta de Dados</h2>
+                    <p>
+                        Os seus dados pessoais e seus dados sensíveis são recebidos por nós no momento da contratação dos nossos produtos e serviços. Isso ocorre quando:
+                    </p>
+                    <ul>
+                        <li>Você preenche os formulários em nossas plataformas digitais;</li>
+                        <li>Você entra em contato com nossas centrais de atendimento;</li>
+                        <li>Você é incluído em nossos sistemas por terceiros intermediários na contratação, como corretores, representantes bancários, estipulantes de seguros, agentes financeiros e parceiros de negócio;</li>
+                        <li>Você navega em nossos canais digitais.</li>
+                    </ul>
+                </section>
+            </div>
+        </main>
+    );
+}
---
+++ src/routes/Sobre/index.tsx
@@ -0,0 +1,26 @@
+export default function Sobre() {
+    return (
+        <main>
+            <div className="titulo_texto">
+                <h1 className="titulo_sobre">Sobre nós</h1>
+                <section className="texto_sobre">
+                    <p className="sobre_textos">
+                        A Porto Seguro é uma das mais renomadas empresas do setor de seguros no Brasil, conhecida por sua excelência em proteger vidas e bens há décadas. Fundada em 1945, a empresa tem uma longa história de compromisso com seus clientes, oferecendo uma ampla gama de produtos e serviços para atender às necessidades de segurança e tranquilidade de indivíduos e empresas.
+                    </p>
+                    <p className="sobre_textos">
+                        Com uma abordagem centrada no cliente, a Porto Seguro tem como missão principal proporcionar segurança e confiança em todas as etapas da vida de seus segurados. Seja oferecendo seguros para automóveis, residências, empresas ou saúde, a empresa se destaca pela qualidade de seus serviços, pela agilidade no atendimento e pela constante busca por inovação.
+                    </p>
+                    <p className="sobre_textos">
+                        Além dos serviços tradicionais de seguros, a Porto Seguro também se destaca por suas iniciativas de responsabilidade social e sustentabilidade. Por meio de programas educacionais, ambientais e culturais, a empresa busca contribuir para o desenvolvimento das comunidades onde atua, demonstrando seu compromisso com o bem-estar social e o meio ambiente.
+                    </p>
+                    <p className="sobre_textos">
+                        Com uma equipe dedicada e especializada, a Porto Seguro está sempre pronta para oferecer soluções personalizadas e adequadas às necessidades de cada cliente. Seja para proteger seu patrimônio, sua saúde ou seu futuro, contar com a Porto Seguro é ter a certeza de estar em boas mãos, em todas as situações.
+                    </p>
+                    <p className="sobre_textos">
+                        Em resumo, a Porto Seguro é mais do que uma empresa de seguros; é uma parceira confiável, que está ao lado de seus clientes, oferecendo segurança, tranquilidade e proteção em todos os momentos da vida.
+                    </p>
+                </section>
+            </div>
+        </main>
+    );
+}

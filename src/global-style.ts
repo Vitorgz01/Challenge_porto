b15836924dc05d9f180a2e0a0f7f13d670c93242
Vitorgz01 <vitorginza123@gmail.com>
2024-09-16 18:56:58 -0300

    criando o global styled

---
+++ src/global-style.ts
@@ -0,0 +1,26 @@
+// import { createGlobalStyle } from "styled-components";
+
+// const GlobalStyle = createGlobalStyle`
+
+// :root {
+//     --cor-texto-botoes: #ffffff;
+//     --cor-botoes: #FD1645;
+//     --cor-primaria: #F1F1F1;
+//     --cor-secundaria: #000034;
+//     --cor-terciaria: #FD1645;
+//     --cor-hover-main: #9c1833;
+//     --cor-hover-cabecalho: #9c1833;
+
+//     --font-titulo: "Poppins", sans-serif;
+//     --font-texto: "Montserrat", sans-serif;
+//     --font-texto-botao: "Jost", sans-serif;
+// }
+
+// *{
+//     margin: 0;
+//     padding: 0;
+// }
+
+// `
+
+// export default GlobalStyle
